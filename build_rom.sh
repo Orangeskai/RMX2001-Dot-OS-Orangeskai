d667327f459b14a1a29efe4dfc550b1b183423ef
Orangeskai <42552126+Orangeskai@users.noreply.github.com>
2021-08-06 15:57:30 +0700

    Update build_rom.sh
    STILL IDK

--- build_rom.sh
+++ build_rom.sh
@@ -8,6 +8,11 @@ repo init --depth=1 -u git://github.com/DotOS/manifest.git -b dot11
 ##git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
 
+# git clone dt,kt,vt
+git clone https://github.com/SamarV-121/android_device_realme_mt6785-common -b lineage-18.1 device/realme/RMX2001
+git clone https://github.com/SamarV-121/android_device_realme_RMX2001 -b lineage-18.1 device/realme/RMX2001
+git clone https://github.com/SamarV-121/android_kernel_realme_RMX2001 -b lineage-18.1 kernel/RMX2001
+
 # build rom
 cd Project
 source build/envsetup.sh
